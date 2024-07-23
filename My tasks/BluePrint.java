e2df7ed80ade6de7775d165e41d04a81998d52c1
Harshiii_F <harshithaf10.xworkz@gmail.com>
2024-07-23 15:02:20 +0100

    23/07/24

---
+++ My tasks/BluePrint.java
@@ -0,0 +1,143 @@
+class Helicopter
+{
+	Helicopter()
+	{
+		System.out.println("created Helicopter");
+	}
+}
+
+class Market
+{
+	Market()
+	{
+		System.out.println("created Market");
+	}
+}
+
+class Mall
+{
+	Mall()
+	{
+		System.out.println("created Mall");
+	}
+}
+
+class Train
+{
+	Train()
+	{
+		System.out.println("created Train");
+	}
+}
+
+class Ring
+{
+	Ring()
+	{
+		System.out.println("created Ring");
+	}
+}
+
+class Auto
+{
+	Auto()
+	{
+		System.out.println("created Auto");
+	}
+}
+
+class Auditorium
+{
+	Auditorium()
+	{
+		System.out.println("created Auditorium");
+	}
+}
+
+class SpaceShip
+{
+	SpaceShip()
+	{
+		System.out.println("created SpaceShip");
+	}
+}
+
+class Alien
+{
+	Alien()
+	{
+		System.out.println("created Alien");
+	}
+}
+
+class Bullet
+{
+	Bullet()
+	{
+		System.out.println("created Bullet");
+	}
+}
+
+class Galaxy
+{
+	Galaxy()
+	{
+		System.out.println("created Galaxy");
+	}
+}
+
+class Spoon
+{
+	Spoon()
+	{
+		System.out.println("created Spoon");
+	}
+}
+
+class BlackHole
+{
+	BlackHole()
+	{
+		System.out.println("created BlackHole");
+	}
+}
+
+class Cake
+{
+	Cake()
+	{
+		System.out.println("created Cake");
+	}
+}
+
+class Wrapper
+{
+	Wrapper()
+	{
+		System.out.println("created Wrapper");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ My tasks/BluePrintRunner.java
@@ -0,0 +1,110 @@
+class BluePrintRunner
+{
+	public static void main(String[] values)
+	{
+		Helicopter helicopter=new Helicopter();
+		Helicopter helicopter1=new Helicopter();
+		Helicopter helicopter2=new Helicopter();
+		Helicopter helicopter3=new Helicopter();
+		Helicopter helicopter4=new Helicopter();
+		
+		System.out.println("===============");
+		Market market=new Market();
+		Market flower=new Market();
+		Market vegitable=new Market();
+		Market fruits=new Market();
+		Market clothes=new Market();
+		flower=vegitable;
+		
+		System.out.println("===============");
+		Mall mall=new Mall();
+		Mall theater=new Mall();
+		Mall shopping=new Mall();
+		Mall glossary=new Mall();
+		Mall food=new Mall();
+		
+		System.out.println("===============");
+		Train train=new Train();
+		Train cells=new Train();
+		Train seats=new Train();
+		Train stations=new Train();
+		Train engine=new Train();
+		
+		System.out.println("===============");
+		Ring ring=new Ring();
+		Ring shape=new Ring();
+		Ring price=new Ring();
+		Ring material=new Ring();
+		Ring color=new Ring();
+		
+		System.out.println("===============");
+		Auto auto=new Auto();
+		Auto metre=new Auto();
+		Auto location=new Auto();
+		Auto wheels=new Auto();
+		Auto quotes=new Auto();
+		
+		System.out.println("===============");
+		Auditorium auditorium=new Auditorium();
+		Auditorium screen=new Auditorium();
+		Auditorium lights=new Auditorium();
+		Auditorium crowd=new Auditorium();
+		Auditorium players=new Auditorium();
+		
+		System.out.println("===============");
+		SpaceShip spaceShip=new SpaceShip();
+		SpaceShip scientist=new SpaceShip();
+		SpaceShip requirements=new SpaceShip();
+		SpaceShip mask=new SpaceShip();
+		SpaceShip ships =new SpaceShip();
+		
+		System.out.println("===============");
+		Alien alien=new Alien();
+		Alien space=new Alien();
+		Alien appearance=new Alien();
+		Alien skinColor=new Alien();
+		Alien outLook=new Alien();
+		
+		System.out.println("===============");
+		Bullet bullet=new Bullet();
+		Bullet cost=new Bullet();
+		Bullet model=new Bullet();
+		Bullet design=new Bullet();
+		Bullet sound=new Bullet();
+		
+		System.out.println("===============");
+		Galaxy galaxy=new Galaxy();
+		Galaxy planets=new Galaxy();
+		Galaxy silence=new Galaxy();
+		Galaxy stars=new Galaxy();
+		Galaxy milkyway=new Galaxy();
+		
+		System.out.println("===============");
+		Spoon spoon=new Spoon();
+		Spoon steel=new Spoon();
+		Spoon plastic=new Spoon();
+		Spoon silver=new Spoon();
+		Spoon gold=new Spoon();
+		
+		System.out.println("===============");
+		BlackHole blackHole=new BlackHole();
+		BlackHole menu=new BlackHole();
+		BlackHole drinks=new BlackHole();
+		BlackHole orders=new BlackHole();
+		BlackHole items=new BlackHole();
+		
+		System.out.println("===============");
+		Cake cake=new Cake();
+		Cake chocolate=new Cake();
+		Cake redvelvet=new Cake();
+		Cake pineapple=new Cake();
+		Cake butterscotch=new Cake();
+		
+		System.out.println("===============");
+		Wrapper wrapper=new Wrapper();
+		Wrapper cover=new Wrapper();
+		Wrapper bag=new Wrapper();
+		Wrapper blanket=new Wrapper();
+		Wrapper sheet=new Wrapper();
+	}
+}
