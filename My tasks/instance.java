a408cc23e98cc3f9c6d72bf3bfe063cb97cce588
Harshiii_F <harshithaf10.xworkz@gmail.com>
2024-07-28 20:24:30 +0100

    instance and default

---
+++ My tasks/instance.java
@@ -0,0 +1,137 @@
+class Charger
+{
+	String company;
+	int pins=2;
+	char type;
+	
+	Charger()
+	{
+		System.out.println("Created Charger");
+	}
+	
+}
+
+class Claw
+{
+	String name="kitten";
+	int claws;
+	boolean tear;
+	
+	Claw()
+	{
+		System.out.println("Created Claw");
+	}
+	
+}
+
+
+class Crow
+{
+	String colour;
+	int wingspan;
+	int lifespan=15;
+	
+	Crow()
+	{
+		System.out.println("Created Crow");
+	}
+	
+}
+
+
+class Clock
+{
+	String shape="round";
+	double price;
+	boolean knock;
+	
+	Clock()
+	{
+		System.out.println("Created Clock");
+	}
+	
+}
+
+
+class Lolipop
+{
+	double price;
+	String colour;
+	String flavour="Mango";
+	
+	Lolipop()
+	{
+		System.out.println("Created Lolipop");
+	}
+	
+}
+
+
+class Jean
+{
+	double price;
+	char size='S';
+	String type;
+	
+	Jean()
+	{
+		System.out.println("Created Jean");
+	}
+	
+}
+
+
+class KeyBoard
+{
+	int count;
+	String shape;
+	boolean works=false;
+	
+	KeyBoard()
+	{
+		System.out.println("Created KeyBoard");
+	}
+	
+}
+
+
+class Mountains
+{
+	String name;
+	String loc="chickmangalore";
+	float height;
+	
+	Mountains()
+	{
+		System.out.println("Created Mountains");
+	}
+	
+}
+
+
+class Stadium
+{
+	String name;
+	String match;
+	String loc="Bangalore";
+	
+	Stadium()
+	{
+		System.out.println("Created Stadium");
+	}
+	
+}
+
+
+class Spray
+{
+	double price;
+	String use;
+	String name="Hit";
+	
+	Spray()
+	{
+		System.out.println("Created Spray");
+	}
+	
+}
