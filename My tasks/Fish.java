d1a624edef0669361d91a482b5ad7c2dc674693e
Harshiii_F <harshithaf10.xworkz@gmail.com>
2024-07-30 19:18:49 +0100

    Constructor with parameter var

---
+++ My tasks/Fish.java
@@ -0,0 +1,141 @@
+class Fish
+{
+	String peru;
+	String color;
+	Fish(String peruLocal,String colorLocal)
+	{
+		System.out.println("Fish created");
+		System.out.println("Name of Fish:"+peru);
+		System.out.println("Fish color:"+color);
+		System.out.println("Fish given name:"+peruLocal);
+		System.out.println("Fish given color:"+colorLocal);
+	}
+}
+
+class Lamp
+{
+	double weight;
+	String size;
+	Lamp(double weightLocal,String sizeLocal)
+	{
+		System.out.println("Lamp created");
+		System.out.println("weight of Lamp:"+weightLocal);
+		System.out.println("size of Lamp is:"+sizeLocal);
+	}
+}
+
+class PrintingMachine
+{
+	double cost;
+	double version;
+	PrintingMachine(double costLocal,double versionLocal)
+	{
+		System.out.println("PrintingMachine created");
+		System.out.println("Cost of PrintingMachine:"+costLocal);
+		System.out.println("Version of PrintingMachine is:"+versionLocal);
+	}
+}
+
+class Lens
+{
+	int sight;
+	String shape;
+	Lens(int sightLocal,String shapeLocal)
+	{
+		System.out.println("Lens created");
+		System.out.println("Sight of Lens:"+sightLocal);
+		System.out.println("Shape of Lens is:"+shapeLocal);
+	}
+}
+class Coil
+{
+	int quantity;
+	double price;
+	Coil(int quantityLocal,double priceLocal)
+	{
+		System.out.println("Coil created");
+		System.out.println("quantity of Coil:"+quantityLocal);
+		System.out.println("price of Coil is:"+priceLocal);
+	}
+}
+
+class Cap
+{
+	int numbers;
+	double amount;
+	String place;
+	
+	Cap(int numbersLocal,double amountLocal,String placeLocal)
+	{
+		System.out.println("Cap created");
+		System.out.println("numbers of Cap:"+numbersLocal);
+		System.out.println("amount of Cap is:"+amountLocal);
+		System.out.println("place of Cap bought is:"+placeLocal);
+	}
+}
+
+class Gold
+{
+	double total;
+	double perGram;
+	String shopName;
+	
+	Gold(double totalLocal,double perGramLocal,String shopNameLocal)
+	{
+		System.out.println("Gold created");
+		System.out.println("carat of Gold:"+totalLocal);
+		System.out.println("perGram for Gold is:"+perGramLocal);
+		System.out.println("shopName of Gold bought is:"+shopNameLocal);
+	}
+}
+
+class Board
+{ 
+	int cement;
+	String material;
+	String Surface;
+	
+	Board(int cementLocal,String materialLocal,String SurfaceLocal)
+	{
+		System.out.println("Board created");
+		System.out.println("cement used for Board:"+cementLocal);
+		System.out.println("material for Board is:"+materialLocal);
+		System.out.println("Surface for Board is:"+SurfaceLocal );
+	}
+}
+
+class LED
+{ 
+	int pieces;
+	String colors;
+	String quality;
+	
+	LED(int piecesLocal,String colorsLocal,String qualityLocal)
+	{
+		System.out.println("LED created");
+		System.out.println("No of Pieces of LED:"+piecesLocal);
+		System.out.println("Colours of LED is:"+colorsLocal);
+		System.out.println("Quality of LED is:"+qualityLocal );
+	}
+}
+
+class SimCard
+{ 
+	int packets;
+	String names;
+	String iccid;
+	
+	SimCard(int packetsLocal,String namesLocal,String iccidLocal)
+	{
+		System.out.println("SimCard created");
+		System.out.println("No of packets of SimCard:"+packetsLocal);
+		System.out.println("Colours of SimCard is:"+namesLocal);
+		System.out.println("Quality of SimCard is:"+iccidLocal );
+	}
+}
+
+
+
+
+
+
---
+++ My tasks/FishRunner.java
@@ -0,0 +1,136 @@
+class FishRunner
+{
+	public static void main(String[] args)
+	{
+		Fish fish=new Fish("Silver","white");
+		Fish fish1=new Fish("anjal","cement");
+		Fish fish2=new Fish("barracuda","grey");
+		Fish fish3=new Fish("carp","white");
+		Fish fish4=new Fish("catfish","black");
+		Fish fish5=new Fish("trout","white");
+		Fish fish6=new Fish("tuna","white");
+		Fish fish7=new Fish("anchovy","gray");
+		Fish fish8=new Fish("halibut","white");
+		Fish fish9=new Fish("grouper","white");
+		Fish fish10=new Fish("cod","white");
+		System.out.println("=======================");
+		
+		Lamp lamp=new Lamp(234,"small");
+		Lamp lamp1=new Lamp(435,"medium");
+		Lamp lamp2=new Lamp(576,"medium");
+		Lamp lamp3=new Lamp(435.5,"medium");
+		Lamp lamp4=new Lamp(284,"small");
+		Lamp lamp5=new Lamp(564,"medium");
+		Lamp lamp6=new Lamp(976,"large");
+		Lamp lamp7=new Lamp(876,"large");
+		Lamp lamp8=new Lamp(976,"large");
+		Lamp lamp9=new Lamp(764,"medium");
+		Lamp lamp10=new Lamp(654,"medium");
+		System.out.println("=======================");
+		
+		PrintingMachine printingMachine=new PrintingMachine(32321,3.1);
+		PrintingMachine printingMachine1=new PrintingMachine(56000,3.2);
+		PrintingMachine printingMachine2=new PrintingMachine(45000,3.4);
+		PrintingMachine printingMachine3=new PrintingMachine(42360,4.1);
+		PrintingMachine printingMachine4=new PrintingMachine(45860,3.21);
+		PrintingMachine printingMachine5=new PrintingMachine(67500,3.51);
+		PrintingMachine printingMachine6=new PrintingMachine(25430,3.9);
+		PrintingMachine printingMachine7=new PrintingMachine(56450,3.8);
+		PrintingMachine printingMachine8=new PrintingMachine(57860,3.901);
+		PrintingMachine printingMachine9=new PrintingMachine(97500,3.6);
+		PrintingMachine printingMachine10=new PrintingMachine(84650,4.2);
+		System.out.println("=======================");
+		
+		Lens lens=new Lens(32,"round");
+		Lens lens1=new Lens(24,"rectangle");
+		Lens lens2=new Lens(35,"oval");
+		Lens lens3=new Lens(19,"square");
+		Lens lens4=new Lens(27,"rectangle");
+		Lens lens5=new Lens(33,"rectangle");
+		Lens lens6=new Lens(37,"round");
+		Lens lens7=new Lens(27,"rectangle");
+		Lens lens8=new Lens(38,"square");
+		Lens lens9=new Lens(47,"rectangle");
+		Lens lens10=new Lens(42,"round");
+		Lens lens11=new Lens(36,"square");
+		System.out.println("=======================");
+		
+		Coil coil=new Coil(5,100);
+		Coil coil1=new Coil(4,80);
+		Coil coil2=new Coil(3,60);
+		Coil coil3=new Coil(6,120);
+		Coil coil4=new Coil(10,200);
+		Coil coil5=new Coil(50,250);
+		Coil coil6=new Coil(8,40);
+		Coil coil7=new Coil(76,1520);
+		Coil coil8=new Coil(15,300);
+		Coil coil9=new Coil(55,110);
+		Coil coil10=new Coil(9,180);
+		System.out.println("=======================");
+		System.out.println("=======================");
+		
+		Cap cap=new Cap(3,456,"mysore");
+		Cap cap1=new Cap(6,565,"kolar");
+		Cap cap2=new Cap(7,4556,"hassan");
+		Cap cap3=new Cap(15,896,"mangalore");
+		Cap cap4=new Cap(18,1456,"udupi");
+		Cap cap5=new Cap(23,8796,"bangalore");
+		Cap cap6=new Cap(13,9156,"mandya");
+		Cap cap7=new Cap(65,48756,"patna");
+		Cap cap8=new Cap(32,8733,"rajasthan");
+		Cap cap9=new Cap(23,2384,"jammu");
+		Cap cap10=new Cap(89,45286,"kashmir");
+		System.out.println("=======================");
+		
+		Gold gold=new Gold(457000,6548,"asha jwellery");
+		Gold gold1=new Gold(567000,6258,"Kamkshi jwellery");
+		Gold gold2=new Gold(987000,7548,"Padma jwellery");
+		Gold gold3=new Gold(465700,6158,"Hema jwellery");
+		Gold gold4=new Gold(988000,5948,"lalitha jwellery");
+		Gold gold5=new Gold(677000,7148,"Leela jwellery");
+		Gold gold6=new Gold(987000,6248,"RR jwellery");
+		Gold gold7=new Gold(656000,6778,"Joyanukas jwellery");
+		Gold gold8=new Gold(127000,5848,"Manish jwellery");
+		Gold gold9=new Gold(657000,7248,"Prema jwellery");
+		Gold gold10=new Gold(877000,6588,"Punitha jwellery");
+		System.out.println("=======================");
+		
+		Board board=new Board(4,"plastic","34*65");
+		Board board1=new Board(34,"steel","34*65");
+		Board board2=new Board(23,"fibre","34*65");
+		Board board3=new Board(65,"plywood","34*65");
+		Board board4=new Board(87,"hardboard","34*65");
+		Board board5=new Board(34,"plastic","34*65");
+		Board board6=new Board(16,"OSB","34*65");
+		Board board7=new Board(25,"medium fibreBoard","34*65");
+		Board board8=new Board(45,"plywood","34*65");
+		Board board9=new Board(15,"hardboard","34*65");
+		Board board10=new Board(84,"OSB","34*65");
+		System.out.println("=======================");
+		
+		LED led=new LED(53,"white","premium");
+		LED led1=new LED(65,"red","medium");
+		LED led2=new LED(45,"green","premium");
+		LED led3=new LED(87,"blue","low");
+		LED led4=new LED(54,"pink","medium");
+		LED led5=new LED(80,"yellow","premium");
+		LED led6=new LED(23,"green","medium");
+		LED led7=new LED(10,"red","premium");
+		LED led8=new LED(25,"pink","low");
+		LED led9=new LED(60,"yellow","premium");
+		LED led10=new LED(67,"white","medium");
+		System.out.println("=======================");
+		
+		SimCard simCard=new SimCard(2,"BSNL","6534673547HF");
+		SimCard simCard1=new SimCard(2,"AIRTEL","6547548766TF");
+		SimCard simCard2=new SimCard(2,"JIO","987238536745HGF");
+		SimCard simCard3=new SimCard(2,"BSNL","352625376526RES");
+		SimCard simCard4=new SimCard(2,"BSNL","6248687568746VBC");
+		SimCard simCard5=new SimCard(2,"AIRTEL","3426565437654FGHG");
+		SimCard simCard6=new SimCard(2,"JIO","76487354653JFG");
+		SimCard simCard7=new SimCard(2,"VI","2574353445465FGH");
+		SimCard simCard8=new SimCard(2,"JIO","87964458765JYB");
+		SimCard simCard9=new SimCard(2,"BSNL","653454764554FHD");
+		SimCard simCard10=new SimCard(2,"AIRTEL","15437548657NBCG");
+	}
+}
